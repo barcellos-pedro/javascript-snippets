9eadbb56428e6abd15dee70fa0a720bdfb267630
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2021-12-24 14:26:57 -0300

    Add more examples

--- arrays.js
+++ arrays.js
@@ -39,3 +39,32 @@ console.log('\n* * * * * * * * * * * * * * * *\n')
 
 console.log('[Reduce]')
 
+let array = [1,2,3,4,5]
+
+console.log('Reduce without user-supplied “reducer” callback function')
+
+// acc = accumulator | curr = current value
+let arraySum = array.reduce((acc, curr) => {
+    console.log(`acc = ${acc} | curr ${curr}`)
+    return acc + curr;
+}, 0)
+// If we don't pass the initial value '0', the method will take the first element
+// In this case it's optional
+
+console.log(`Array sum = ${arraySum}`)
+
+console.log('Reduce with user-supplied “reducer” callback function')
+
+let sum = (previousValue, currentValue) => previousValue + currentValue;
+
+console.log(array.reduce(sum));
+
+console.log('\nMore examples on reduce\n')
+
+let objSum = [{ x: 1 }, { x: 2 }, { x: 3 }].reduce((acc, curr) => {
+    // console.log('acc\n', acc)
+    // console.log('curr\n', curr.x)
+    return acc + curr.x;
+}, 0)
+
+console.log(`objSum total = ${objSum}`)
--- fetch.js
+++ fetch.js
@@ -1,4 +1,5 @@
-require('fetch')
+// Inside browser's devtools we can test without error
+// On vscode or terminal node won't find 'fetch' object
 
 async function getUserInfo(username, callback) {
     let resultado = await fetch(`https://api.github.com/users/${username}`).then(data => data.json());
